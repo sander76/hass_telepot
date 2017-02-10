6da3934ec5970b2786f86124f706e17c7076dcb2
sanders76 <s.teunissen@gmail.com>
2017-02-10 15:41:56 +0100

    first commit

---
+++ hass_telepot.py
@@ -0,0 +1,119 @@
+"""A telegram bot which allows you to send and receive commands."""
+
+from homeassistant.const import EVENT_HOMEASSISTANT_START, \
+    EVENT_HOMEASSISTANT_STOP
+from homeassistant.helpers.script import Script
+from homeassistant.helpers import config_validation as cv
+import logging
+
+REQUIREMENTS = [
+    'telepot==10.4'
+]
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = 'hass_telepot'
+BOT_TOKEN = 'bot_token'
+ALLOWED_CHAT_IDS = 'allowed_chat_ids'
+COMMANDS = 'commands'
+
+COMMAND = 'command'
+SCRIPT = 'script'
+RESPONSE = 'response'
+RESPONSE_TEXT = 'text'
+RESPONSE_KEYBOARD = 'keyboard'
+
+
+def setup(hass, config):
+    """Setup is called when Home Assistant is loading our component."""
+    logger = logging.getLogger(__name__)
+    import telepot
+
+    bot_token = config[DOMAIN].get(BOT_TOKEN)
+    allowed_chat_ids = config[DOMAIN].get(ALLOWED_CHAT_IDS)
+
+    # instance the Telegram bot
+    bot = telepot.Bot(bot_token)
+    # Build a list of telegram commands.
+    commands = [Instruction(hass, bot, _command) for _command in
+                config[DOMAIN].get(COMMANDS)]
+
+    def _start_bot(_event):
+        bot.message_loop(handle)
+
+    def _stop_bot(_event):
+        """Stops the bot. Unfortunately no clean stopping of
+        the telepot instance available yet (?)"""
+        pass
+
+    hass.bus.listen_once(
+        EVENT_HOMEASSISTANT_START,
+        _start_bot
+    )
+    hass.bus.listen_once(
+        EVENT_HOMEASSISTANT_STOP,
+        _stop_bot
+    )
+
+    def get_command(command):
+        for _cmd in commands:
+            if _cmd.command == command:
+                return _cmd
+        raise UserWarning("telegram command not found.")
+
+    def handle(msg):
+        """Callback function which handles incoming telegram messages."""
+
+        # glance to get some meta on the message
+        content_type, chat_type, chat_id = telepot.glance(msg)
+
+        # we only want to process text messages from our specified chat
+        if (content_type == 'text') and (chat_id in allowed_chat_ids):
+            command = msg['text']
+            try:
+                _cmd = get_command(command)
+            except UserWarning as ex:
+                logger.error(ex)
+                raise
+            _cmd.execute(chat_id)
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
+
+
+class Instruction:
+    def __init__(self, hass, bot, command):
+        self.hass = hass
+        self.bot = bot
+        self.script = None
+        self.response = None
+        self.command = command[COMMAND]
+
+        _script = command.get(SCRIPT, None)
+        if _script is not None:
+            self.script = Script(hass, cv.SCRIPT_SCHEMA(_script))
+
+        _response = command.get(RESPONSE, None)
+        if _response is not None:
+            self.response = {}
+            _text = _response.get(RESPONSE_TEXT, None)
+            _keyboard = _response.get(RESPONSE_KEYBOARD, None)
+            if _text:
+                self.response[RESPONSE_TEXT] = _text
+            if _keyboard:
+                self.response[RESPONSE_KEYBOARD] = {
+                    "keyboard": [],
+                    "resize_keyboard": True,
+                    "one_time_keyboard": True
+                }
+                for _key in _keyboard:
+                    self.response[RESPONSE_KEYBOARD]["keyboard"].append(
+                        [{"text": _key}]
+                    )
+
+    def execute(self, chat_id):
+        if self.script:
+            self.script.run()
+        if self.response:
+            self.bot.sendMessage(chat_id,
+                                 self.response[RESPONSE_TEXT],
+                                 reply_markup=self.response[RESPONSE_KEYBOARD])
---
+++ readme.md
@@ -0,0 +1,36 @@
+# hass_telepot
+
+WORK IN PROGRESS
+
+A home assistant custom component which allows for sending and receiving telegram commands to control HomeAsisstant.
+
+## Installation
+Put the `hass_telepot.py` file in your `custom_components` folder which should by default be in the same folder as your `configuration.yaml` file. Create the folder if it is not there.
+
+## Configuration
+
+Put the following in your `configuration.yaml` file.
+
+```yamlex
+hass_telepot:
+    bot_token : <your bot token>
+    allowed_chat_ids :
+    -   your chat id(s)
+    commands:
+    -   command: /away # the command you send using your telegram client.
+        script: # a hass script you activate when you send the above command.
+            service: input_boolean.turn_on
+            entity_id: input_boolean.away
+    -   command: /athome
+        script:
+            service: input_boolean.turn_off
+            entity_id: input_boolean.away
+    -   command: /presence
+        response: # Response you send back to the client.
+            text: Please choose whether you are at home or not # Text response
+            keyboard: 
+            # Keyboard which is displayed in your client with available buttons to press.
+            -   /away
+            -   /athome
+```
+
